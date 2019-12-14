a0bcab9b49a5f9df41c3240b7066e3ab67c571eb
Heiner Wolf <git@heinerwolf.de>
2019-12-14 20:02:43 +0100

    main sln

---
+++ galdev/galdev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace galdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
