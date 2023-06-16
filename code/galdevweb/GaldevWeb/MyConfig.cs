8463c0ad0d61e54a4939ca18f034a373a1b3c502
Heiner Wolf <git@heinerwolf.de>
2023-06-16 21:15:20 +0200

    first dummy entry

--- code/galdevweb/GaldevWeb/MyConfig.cs
+++ code/galdevweb/GaldevWeb/MyConfig.cs
@@ -5,5 +5,6 @@ namespace GaldevWeb
     public class MyConfig : MemoryCallbackConfig
     {
         public string AppName = "GaldevWeb";
+        public string IndexPath = "../../../data/index.yaml";
     }
 }
