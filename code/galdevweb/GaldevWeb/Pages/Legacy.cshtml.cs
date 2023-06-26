03c0ac0ae296a4efe5aacfb24948b0c3cc426a51
Heiner Wolf <git@heinerwolf.de>
2023-06-26 23:49:57 +0200

    Legacy

---
+++ code/galdevweb/GaldevWeb/Pages/Legacy.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GaldevWeb.Pages
+{
+    public class LegacyModel : GaldevPageModel
+    {
+        public I18nTimelines _timelines;
+        public TimelineEntry Entry = new TimelineEntry("(no-name)", "(no-year)", "(no-title)", new string[0]);
+
+        public LegacyModel(GaldevApp app, I18nTimelines timelines) : base(app, "Legacy")
+        {
+            _timelines = timelines;
+        }
+
+        public IActionResult OnGet(string code)
+        {
+            if (Is.Value(code)) {
+                var prefix = code.Substring(0, 1);
+                var year = code.Substring(1);
+
+                var lang = prefix switch {
+                    "e" => "en",
+                    _ => "de",
+                };
+
+                var timeline = _timelines.GetEntries(lang);
+                var entry = timeline.GetEntryByYear(year);
+                var seoTitle = entry.SeoTitle;
+                return Redirect($"/Timeline/{seoTitle}");
+            }
+            return NotFound();
+        }
+
+    }
+}
--- code/galdevweb/GaldevWeb/Timeline.cs
+++ code/galdevweb/GaldevWeb/Timeline.cs
@@ -1,4 +1,6 @@
-﻿namespace GaldevWeb
+﻿using System.Xml.Linq;
+
+namespace GaldevWeb
 {
     public class Timeline : Dictionary<string, TimelineEntry>
     {
@@ -22,7 +24,7 @@
             if (this.ContainsKey(name)) {
                 return this[name];
             }
-            throw new KeyNotFoundException($"Timeline entry '{name}' not found");
+            throw new KeyNotFoundException($"Timeline entry name='{name}' not found");
         }
 
         public IEnumerable<string> GetNames()
@@ -33,6 +35,16 @@
         public Timeline GetEntries()
         {
             return this;
+        }
+
+        internal TimelineEntry GetEntryByYear(string year)
+        {
+            foreach (var entry in Values) {
+                if (entry.Year == year) {
+                    return entry;
+                }
+            }
+            throw new KeyNotFoundException($"Timeline entry year='{year}' not found");
         }
     }
 }
