c256a4537e9a3fe3a369218d9bff44de724427e5
Heiner Wolf <git@heinerwolf.de>
2023-07-28 23:42:53 +0200

    show SearchTerm

--- code/galdevweb/GaldevWeb/Pages/Find.cshtml.cs
+++ code/galdevweb/GaldevWeb/Pages/Find.cshtml.cs
@@ -4,6 +4,7 @@ namespace GaldevWeb.Pages
     {
         public TimelineIndex _timelines;
         public TimelineEntryList List = new();
+        public string SearchTerm = "";
 
         public FindModel(GaldevApp app, TimelineIndex timelines) : base(app, "List")
         {
@@ -15,11 +16,11 @@ namespace GaldevWeb.Pages
             var lang = GetLangFromCultureName(UiCultureName);
             Log.Info("", new LogData { [nameof(lang)] = lang });
             var timeline = _timelines.GetSeriesForLanguage(lang);
-            var searchTerm = term.ToLower();
+            SearchTerm = term.ToLower();
 
             foreach (var kv in timeline) {
                 var entry = kv.Value;
-                if (entry.FullTextForSearch.ToLower().Contains(searchTerm)) {
+                if (entry.FullTextForSearch.ToLower().Contains(SearchTerm)) {
                     List.Add(entry);
                 }
             }
