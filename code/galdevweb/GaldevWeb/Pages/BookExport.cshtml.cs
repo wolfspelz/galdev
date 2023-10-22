9b8017fbf5ce291cb7f010d38d9780dca9161527
Heiner Wolf <git@heinerwolf.de>
2023-10-22 20:53:52 +0200

    BookExport

---
+++ code/galdevweb/GaldevWeb/Pages/BookExport.cshtml.cs
@@ -0,0 +1,18 @@
+﻿namespace GaldevWeb.Pages
+{
+    public class BookExportModel : GaldevPageModel
+    {
+        public TimelineEntryList List = new();
+
+        public BookExportModel(GaldevApp app) : base(app, "BookExport")
+        {
+        }
+
+        public void OnGet()
+        {
+            var lang = GetLangFromCultureName(UiCultureName);
+            Log.Info("");
+            List = Timeline.GetFilteredList(entry => !entry.Tags.Contains("_nobook"));
+        }
+    }
+}
