73ebadb23840f113442d5699ea70e6079a64b693
Heiner Wolf <git@heinerwolf.de>
2023-11-11 14:23:10 +0100

    book export w/ limits

--- code/galdevweb/GaldevWeb/Pages/BookExport.cshtml.cs
+++ code/galdevweb/GaldevWeb/Pages/BookExport.cshtml.cs
@@ -8,11 +8,34 @@
         {
         }
 
-        public void OnGet()
+        public void OnGet(int start = -1, int end = -1, int min = -1)
         {
             var lang = GetLangFromCultureName(UiCultureName);
-            Log.Info("");
-            List = Timeline.GetFilteredList(entry => !entry.Tags.Contains("_nobook"));
+            Log.Info($"min={min} start={start} end={end}");
+            List = Timeline.GetFilteredList(entry => {
+                var match = true;
+                match = match && !entry.Tags.Contains("_nobook");
+                if (match) {
+                    if (start > 0) {
+                        if (int.TryParse(entry.Year, out var year)) {
+                            match = match && year >= start;
+                        } else { match = false; }
+                    }
+                }
+                if (match) {
+                    if (end > 0) {
+                        if (int.TryParse(entry.Year, out var year)) {
+                            match = match && year <= end;
+                        } else { match = false; }
+                    }
+                }
+                if (match) {
+                    if (min > 0) {
+                        match = match && entry.TextLen >= min;
+                    }
+                }
+                return match;
+            });
         }
     }
 }
