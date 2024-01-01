c6b2dc4acd5506db52f36965bf4ab33f30d37e8e
Heiner Wolf <git@heinerwolf.de>
2024-01-02 00:03:30 +0100

    index min len from 1000 to 300

--- code/galdevweb/GaldevWeb/GaldevConfig.cs
+++ code/galdevweb/GaldevWeb/GaldevConfig.cs
@@ -8,7 +8,7 @@
         //public string DataIndexPath = "../../../../../../data/index.yaml";
         //public string BlogIndexPath = "../../../../GaldevWeb/wwwroot/blog/index.yaml";
         public string NotFoundImagePath = "wwwroot/images/site/NotFound.jpg";
-        public int ListMinTextLength = 1000;
+        public int ListMinTextLength = 300;
         public int EntryPageTextLength = 4000;
     }
 }
