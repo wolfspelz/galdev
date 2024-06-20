20935a1f3e77943864187817c93604d011dfba7b
Heiner Wolf <git@heinerwolf.de>
2024-06-20 12:47:34 +0200

    dev StageCheck -> StageTest Corebridge Energy tweak

--- code/galdevweb/GaldevWeb/Pages/StageCheck.cshtml.cs
+++
@@ -1,9 +0,0 @@
-namespace GaldevWeb.Pages
-{
-    public class StageCheckModel : GaldevPageModel
-    {
-        public StageCheckModel(GaldevApp app) : base(app, "StageCheck")
-        {
-        }
-    }
-}
