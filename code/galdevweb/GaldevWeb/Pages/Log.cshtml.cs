bfd7407a30c3ed10236ce4db379349240afc3f5a
Heiner Wolf <git@heinerwolf.de>
2024-11-09 13:12:03 +0100

    colored log optional

--- code/galdevweb/GaldevWeb/Pages/Log.cshtml.cs
+++ code/galdevweb/GaldevWeb/Pages/Log.cshtml.cs
@@ -5,14 +5,16 @@ namespace GaldevWeb.Pages
     public class LogModel : GaldevPageModel
     {
         public int NumLines = 0;
+        public bool Colored = false;
 
         public LogModel(GaldevApp app) : base(app, "Log")
         {
         }
 
-        public void OnGet(int lines = 0)
+        public void OnGet(int lines = 0, bool colored = false)
         {
-            //Log.Info($"");
+            Colored = colored;
+
             if (lines > 0) {
                 NumLines = lines;
             } else {
